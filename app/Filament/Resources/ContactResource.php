a95d50ec50969270fae6d1e5be9b76d5e9d5d31a
Ngab-Rio <mamanadifirmansyah@gmail.com>
2025-07-07 22:04:45 +0700

    update: filament

--- app/Filament/Resources/ContactResource.php
+++ app/Filament/Resources/ContactResource.php
@@ -49,7 +49,7 @@ class ContactResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
